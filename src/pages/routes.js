87f967f0ec999179fe0eb652edd416cd039854e2
Arthur Bernardo <arthurutschgg@gmail.com>
2022-05-04 19:38:37 -0300

    feat: add presentation screen

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,44 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MeusPets from './MeusPets/index';
 import Profile from './Profile/index';
 import { Entypo, Feather } from '@expo/vector-icons'
+import Presentation from "./presentation/Presentation";
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function Routes(){
-    return(
-        <Tab.Navigator
-            screenOptions={{
-                tabBarStyle:{
-                    backgroundColor: '#fff',
-                    borderTopColor:'transparent',
-                },
-                tabBarActiveTintColor: '#08B139',
-                // tabBarInactiveTintColor:
-                
-            }}
-        >
-            <Tab.Screen 
-            name="Meus Pets"
-            component={MeusPets}
-            options={{
-                tabBarIcon:({size, color}) => (
-                    <Entypo name='home' size={size} color={color}/>
-                )
-            }}
-            />
 
-            <Tab.Screen 
-            name="Perfil" 
-            component={Profile}
-            options={{
-                tabBarIcon:({size, color}) => (
-                    <Feather name='user' size={size} color={color}/>
-                )
-            }}
-            />
-        </Tab.Navigator>
+    const [teste, setTeste] = useState(false)
+    return(
+        <>
+        {!teste ? (
+                    <Stack.Navigator>
+                        <Stack.Screen name="Home" component={Presentation} />
+                        <Stack.Screen name="seila" component={Presentation} />
+                    </Stack.Navigator>
+        ) : (
+            <>
+                <Tab.Navigator
+                    screenOptions={{
+                        tabBarStyle:{
+                            backgroundColor: '#fff',
+                            borderTopColor:'transparent',
+                        },
+                        tabBarActiveTintColor: '#08B139',
+                        // tabBarInactiveTintColor:
+
+                    }}
+                >
+                    <Tab.Screen
+                        name="Meus Pets"
+                        component={MeusPets}
+                        options={{
+                            tabBarIcon:({size, color}) => (
+                                <Entypo name='home' size={size} color={color}/>
+                            )
+                        }}
+                    />
+
+                    <Tab.Screen
+                        name="Perfil"
+                        component={Profile}
+                        options={{
+                            tabBarIcon:({size, color}) => (
+                                <Feather name='user' size={size} color={color}/>
+                            )
+                        }}
+                    />
+                </Tab.Navigator>
+            </>
+    )}
+        </>
     )
 }
