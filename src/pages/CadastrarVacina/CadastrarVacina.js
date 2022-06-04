ce4e3dfefcfe8f82a84f2735e18cc7cf90240c08
Aline Silva <77028762+AlineSilvaDev@users.noreply.github.com>
2022-06-04 19:55:49 -0300

    Add files via upload

--- src/pages/CadastrarVacina/CadastrarVacina.js
+++ src/pages/CadastrarVacina/CadastrarVacina.js
@@ -2,12 +2,42 @@ import React, { useState } from "react";
 import { View, ScrollView } from "react-native";
 import { TextInput, Button } from "react-native-paper";
 
-const CadastrarVacina = () => {
+const CadastrarVacina = ({ navigation }) => {
   const [nome, setNome] = useState("");
   const [vacina, setVacina] = useState("");
   const [idade, setIdade] = useState("");
   const [data, setData] = useState("");
   const [dose, setDose] = useState("");
+
+  const onSubmit = async ({ formData }) => {
+    let token = await AsyncStorage.getItem("token");
+
+    const data = new FormData();
+    data.append("nome", { input: nome });
+    data.append("vacina", { input: vacina });
+    data.append("idade", { input: idade });
+    data.append("data", { input: data });
+    data.append("dose", { input: dose });
+
+    axios
+      .post("api", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json",
+          "Content-Type": "multipart/form-data",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Access-Control-Allow-Credentials": "true",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <View>
@@ -41,7 +71,7 @@ const CadastrarVacina = () => {
 
         <Button
           title={"Cadastrar vacina"}
-          onPress={() => console.log("Pressed")}
+          onPress={() => onSubmit({ formData })}
         />
       </ScrollView>
     </View>
