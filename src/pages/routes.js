3a71143500188da9b7aa27bb60e77fe62580707e
Aline Silva <77028762+AlineSilvaDev@users.noreply.github.com>
2022-06-05 13:21:02 -0300

    Add files via upload

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,65 +1,64 @@
-import React, {useState} from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import MeusPets from './MeusPets/index';
-import Profile from './Profile/index';
-import { Entypo, Feather } from '@expo/vector-icons'
+import React, { useState } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MeusPets from "./MeusPets/index";
+import Profile from "./Profile/index";
+import { Entypo, Feather } from "@expo/vector-icons";
 import Presentation from "./presentation/Presentation";
-import {CreateAccount} from "./CreateAccount/CreateAccount";
+import { CreateAccount } from "./CreateAccount/CreateAccount";
 import CadastrarPet from "./CadastrarPet/CadastrarPet";
-import {Login} from "./Login/Login";
+import { Login } from "./Login/Login";
+import CadastrarVacina from "./CadastrarVacina/CadastrarVacina";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-export default function Routes(){
-
-    const [teste, setTeste] = useState(false)
-    return(
+export default function Routes() {
+  const [teste, setTeste] = useState(false);
+  return (
+    <>
+      {!teste ? (
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Presentation} />
+          <Stack.Screen name="Criar conta" component={CreateAccount} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Cadastrar Pets" component={CadastrarPet} />
+          <Stack.Screen name="Cadastrar Vacina" component={CadastrarVacina} />
+        </Stack.Navigator>
+      ) : (
         <>
-        {!teste ? (
-                    <Stack.Navigator>
-                        <Stack.Screen name="Home" component={Presentation} />
-                        <Stack.Screen name="Criar conta" component={CreateAccount} />
-                        <Stack.Screen name="Login" component={Login} />
-                        <Stack.Screen name="Cadastrar Pets" component={CadastrarPet} />
-                    </Stack.Navigator>
-        ) : (
-            <>
+          <Tab.Navigator
+            screenOptions={{
+              tabBarStyle: {
+                backgroundColor: "#fff",
+                borderTopColor: "transparent",
+              },
+              tabBarActiveTintColor: "#08B139",
+              // tabBarInactiveTintColor:
+            }}
+          >
+            <Tab.Screen
+              name="Meus Pets"
+              component={MeusPets}
+              options={{
+                tabBarIcon: ({ size, color }) => (
+                  <Entypo name="home" size={size} color={color} />
+                ),
+              }}
+            />
 
-                <Tab.Navigator
-                    screenOptions={{
-                        tabBarStyle:{
-                            backgroundColor: '#fff',
-                            borderTopColor:'transparent',
-                        },
-                        tabBarActiveTintColor: '#08B139',
-                        // tabBarInactiveTintColor:
-
-                    }}
-                >
-                    <Tab.Screen
-                        name="Meus Pets"
-                        component={MeusPets}
-                        options={{
-                            tabBarIcon:({size, color}) => (
-                                <Entypo name='home' size={size} color={color}/>
-                            )
-                        }}
-                    />
-
-                    <Tab.Screen
-                        name="Perfil"
-                        component={Profile}
-                        options={{
-                            tabBarIcon:({size, color}) => (
-                                <Feather name='user' size={size} color={color}/>
-                            )
-                        }}
-                    />
-                </Tab.Navigator>
-            </>
-    )}
+            <Tab.Screen
+              name="Perfil"
+              component={Profile}
+              options={{
+                tabBarIcon: ({ size, color }) => (
+                  <Feather name="user" size={size} color={color} />
+                ),
+              }}
+            />
+          </Tab.Navigator>
         </>
-    )
+      )}
+    </>
+  );
 }
